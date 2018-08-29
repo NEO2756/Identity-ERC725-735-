c9b4d1f99d3a060165d758d5f684e359f5d352f1
Sandeep Sharma <sandeep.jack2756@gmail.com>
2018-08-29 19:40:21 +0530

    Initial Draft implemetation of ERC725/735

---
+++ contracts/ClaimProxy.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.4.24;
+
+import "./IdentityProxy.sol";
+import "./ERC735.sol";
+
+        // pendingClaims[claimRequestId].topic = _topic;
+        // pendingClaims[claimRequestId].scheme = _scheme;
+        // pendingClaims[claimRequestId].issuer = _issuer;
+        // pendingClaims[claimRequestId].signature = _signature;
+        // pendingClaims[claimRequestId].data = _data;
+        // pendingClaims[claimRequestId].uri = _uri;
+
+contract ClaimProxy is ERC735 {
+
+    event Log(bytes data);
+
+    mapping (bytes32 => Claim) claims;
+    mapping (uint256 => Claim) pendingClaims;
+    mapping (uint256 => bytes32[]) claimIdsByTopic;
+    uint256 private claimRequestId = 0;
+
+    IdentityProxy public identityAddress; // = new IdentityProxy(1);
+
+    constructor(address identity) {
+        identityAddress = IdentityProxy(identity);
+    }
+
+    function getClaim(bytes32 _claimId) public constant returns(uint256 topic, uint256 scheme, address issuer, bytes signature, bytes data, string uri) {
+      Claim claim = claims[_claimId];
+      return (claim.topic, claim.scheme, claim.issuer, claim.signature, claim.data, claim.uri);
+    }
+
+    function getClaimIdsByTopic(uint256 _topic) public constant returns(bytes32[] claimIds) {
+        return claimIdsByTopic[_topic];
+    }
+
+    //bytes32 claimId = keccak256(issuer_address, topic);
+    function addPendingClaim(uint256 _topic, uint256 _scheme, address _issuer, bytes _signature, bytes _data, string _uri) internal returns (uint256) {
+
+        bytes memory txData = abi.encodeWithSignature("addClaim(uint256,uint256,address,bytes,bytes,string)", _topic,_scheme, _issuer, _signature, _data, _uri);
+        return identityAddress.addExecution(address(this), 0, txData);
+        //emit Log(txData);
+    }
+    /* This SHOULD create a pending claim, which SHOULD to
+    be approved or rejected by n of m approve calls from keys of purpose 1
+    if its self claim, no approve is required.
+    Returns claimRequestId: COULD be send to the approve function, to approve or reject this claim.
+    Triggers if the claim is new Event and approval process exists: ClaimRequested
+    Triggers if the claim is new Event and is added: ClaimAdded
+    Triggers if the claim index existed Event: ClaimChanged  */
+    function addClaim(uint256 _topic, uint256 _scheme, address _issuer, bytes _signature, bytes _data, string _uri) public returns (uint256 claimRequestId) {
+
+      uint256 id = addPendingClaim(_topic, _scheme, _issuer, _signature, _data, _uri);
+      ClaimRequested(id, _topic, _scheme, _issuer, _signature, _data, _uri);
+
+      //No approval for self claim, else approval process decide the addition of claims
+      if (msg.sender != address(identityAddress)) {
+          require(identityAddress.keyHasPurpose(keccak256(msg.sender), 1));
+      }
+        bytes32 claimId = keccak256(_issuer, _topic);
+        claims[claimId].topic = _topic;
+        claims[claimId].scheme = _scheme;
+        claims[claimId].issuer = _issuer;
+        claims[claimId].signature = _signature;
+        claims[claimId].data = _data;
+        claims[claimId].uri = _uri;
+        ClaimAdded(claimId, _topic, _scheme, _issuer, _signature, _data, _uri);
+      return id;
+    }
+    //function changeClaim(bytes32 _claimId, uint256 _topic, uint256 _scheme, address _issuer, bytes _signature, bytes _data, string _uri) returns (bool success);
+    //function removeClaim(bytes32 _claimId) public returns (bool success);
+}
---
+++ contracts/IdentityProxy.sol
@@ -0,0 +1,143 @@
+pragma solidity ^0.4.24;
+
+import "./ERC725.sol";
+
+contract IdentityProxy is ERC725 {
+
+  //we will move this in ERC725.sol afterwards.
+  uint256 constant MANAGEMENT_KEY = 1;
+  uint256 constant ACTION_KEY = 2;
+  uint256 constant CLAIM_SIGNER_KEY = 3;
+  uint256 constant ENCRYPTION_KEY = 4;
+  uint256 constant MAX_PURPOSE_LENGTH = 4;
+
+  uint256 private multisigRequirement;
+
+  event ExecutionFailed(uint256, string);
+
+
+  struct transaction {
+    address to;
+    bytes data;
+    uint256 value;
+    uint256 requirement;
+    bool executed;
+  }
+
+//   struct Key {
+//     uint256[] purposes;
+//     uint256 keyType;
+//     bytes32 key;
+//   }
+
+  mapping(bytes32 => Key) private keys;
+  mapping(uint256 => bytes32[]) keysByPurpose;
+  mapping(uint256 => transaction) transactions;
+  address public identity;
+
+  uint256 private executionId = 0;
+
+
+  constructor(uint256 req) {
+    bytes32 key = keccak256(msg.sender);
+    keys[key].key = key;
+    keys[key].purposes.push(MANAGEMENT_KEY);
+    keys[key].keyType = 1;
+    multisigRequirement = req;
+    emit KeyAdded(key, MANAGEMENT_KEY, 1);
+  }
+
+  function getKey(bytes32 _key) public constant returns(uint256[] purposes, uint256 keyType, bytes32 key) {
+    return (keys[_key].purposes, keys[_key].keyType, keys[_key].key);
+  }
+
+  function keyHasPurpose(bytes32 _key, uint256 purpose) constant returns(bool exists) {
+    for (uint256 i = 0 ; i < keys[_key].purposes.length; i++) {
+      if (keys[_key].purposes[i] == purpose) return true;
+    }
+    return false;
+  }
+
+  function getKeysByPurpose(uint256 _purpose) public constant returns(bytes32[] keys) {
+      return keysByPurpose[_purpose];
+  }
+
+  function addKey(bytes32 _key, uint256 _purpose, uint256 _keyType) public returns (bool success) {
+    require(_key != 0);
+    require(_purpose <= MAX_PURPOSE_LENGTH);
+
+    //Only MANAGEMENT_KEY holder can directly add the key.
+    if (msg.sender != address(this)) {
+        require(keyHasPurpose(keccak256(msg.sender), MANAGEMENT_KEY));
+    }
+
+    keys[_key].key = _key;
+    keys[_key].purposes.push(_purpose);
+    keys[_key].keyType = _keyType;
+
+    KeyAdded(_key, _purpose,_keyType);
+    return true;
+  }
+
+  function addExecution(address _to, uint256 _value, bytes _data) public returns (uint256) {
+      //transactions[executionId].nonce++;
+      transactions[executionId].to = _to;
+      transactions[executionId].value = _value;
+      transactions[executionId].data = _data;
+      transactions[executionId].requirement = 0; //lets assume 1 of 1 multisig right now.
+      transactions[executionId].executed = false;
+      executionId++;
+      return executionId - 1;
+  }
+
+  function execute(address _to, uint256 _value, bytes _data) public returns (uint256 executionId) {
+        //call when execute is successfuly called
+        bool status = false;
+        executionId = addExecution(_to, _value, _data);
+        ExecutionRequested(executionId, _to, _value, _data);
+
+        if (keyHasPurpose(keccak256(msg.sender), MANAGEMENT_KEY)) { // || keyHasPurpose(keccak256(msg.sender), ACTION_KEY)) {
+           status = approve(executionId, true);
+        }
+        return executionId; //return execution id for someone listening to execution requested event.
+  }
+
+
+
+/* Approves an execution or claim addition.
+This SHOULD require n of m approvals of keys purpose 1, if the _to of
+the execution is the identity contract itself, to successfull approve an execution.
+And COULD require n of m approvals of keys purpose 2, if the _to of the execution is another
+contract, to successfull approve an execution. */
+
+function approve(uint256 _id, bool _approve) public returns (bool success) {
+
+  bool status = false;
+  emit Approved(_id, _approve); //must be called when approve is successfully called
+  require (transactions[_id].executed == false);
+  if (_approve) {
+    if (transactions[_id].to == address(this)) {  //Action on identity contract
+      require(keyHasPurpose(keccak256(msg.sender), MANAGEMENT_KEY)); //require purpose = 1
+      transactions[_id].requirement++;
+      if (transactions[_id].requirement >= multisigRequirement) {
+        status = transactions[_id].to.call.value(transactions[_id].value)(transactions[_id].data);
+        transactions[_id].executed = true;
+      }
+      return true; //need more approvals.
+      } else if (keyHasPurpose(keccak256(msg.sender), ACTION_KEY)) { //anything else u atleast need ACTION key
+        transactions[_id].requirement++;
+        status = transactions[_id].to.call.value(transactions[_id].value)(transactions[_id].data);
+        transactions[_id].executed = true;
+      }
+    }
+    else //Reject claim addition to this identity
+    {
+      status = false;
+      transactions[_id].executed = true;
+    }
+    if (status) {
+      Executed(_id, transactions[_id].to, transactions[_id].value, transactions[_id].data);
+    }
+    return status;
+  }
+}
